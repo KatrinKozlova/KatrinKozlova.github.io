c2acd33ddc3d521d96206484ae5a67d4ce155ee9
KatrinKozlova <43003553+KatrinKozlova@users.noreply.github.com>
2020-07-30 17:24:05 +0300

    Commit bicycle

---
+++ bicycle/js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+	$('.carousel__inner').slick({
+        speed: 1200,
+        dots: true,
+        arrows: false
+    });
+
+    //scroll
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+		const _href = $(this).attr("href");
+		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+		return false;
+    });
+    
+    //form submit
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('form').trigger('reset');
+        });
+        return false;
+	});
+
+});
